c519370cfd799a6c321865c42336c88bfec9d58f
Martin Robinson <martinjrobins@gmail.com>
2017-10-09 11:14:31 +0100

    update to new pints

--- fit_pom.py
+++ fit_pom.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.insert(0, 'pints')
+sys.path.insert(0, 'pints/problems/electrochemistry')
+sys.path.insert(0, 'build')
 import pints
 import electrochemistry
 import pickle
@@ -8,7 +12,6 @@ import matplotlib as mpl
 import pylab as plt
 import numpy.fft as fft
 from math import pi
-
 
 filename = 'POMGCL_6020104_1.0M_SFR_d16.txt'
 
@@ -44,159 +47,117 @@ dim_params = {
     }
 
 
-model = electrochemistry.POMModel(dim_params)
-data = electrochemistry.TimeData(filename,model,ignore_begin_samples=5,ignore_end_samples=0)
+poms_model = electrochemistry.POMModel(dim_params)
+data = electrochemistry.ECTimeData(filename,poms_model,ignore_begin_samples=5,ignore_end_samples=0)
 
-I,t = model.simulate(use_times=data.time)
-
-print 'data loglikelihood is currently',data.log_likelihood(I,0.5)
-
-# specify bounds for parameters
-prior = hobo.Prior()
-e0_buffer = 0.1*(model.params['Estart'] - model.params['Ereverse'])
-E0 = 0.5*(model.params['E01'] + model.params['E02'])
-E0_diff = 1
-prior.add_parameter('E01',hobo.Normal(E0,(2*E0_diff)**2),
-        model.params['Ereverse']+e0_buffer,model.params['Estart']-e0_buffer)
-prior.add_parameter('E02',hobo.Normal(E0,(2*E0_diff)**2),
-        model.params['Ereverse']+e0_buffer,model.params['Estart']-e0_buffer)
-E1 = 0.5*(model.params['E11'] + model.params['E12'])
-E1_diff = 1
-prior.add_parameter('E11',hobo.Normal(E1,(2*E1_diff)**2),
-        model.params['Ereverse']+e0_buffer,model.params['Estart']-e0_buffer)
-prior.add_parameter('E12',hobo.Normal(E1,(2*E1_diff)**2),
-        model.params['Ereverse']+e0_buffer,model.params['Estart']-e0_buffer)
-E2 = 0.5*(model.params['E21'] + model.params['E22'])
-E2_diff = 1
-prior.add_parameter('E21',hobo.Normal(E2,(2*E2_diff)**2),
-        model.params['Ereverse']+e0_buffer,model.params['Estart']-e0_buffer)
-prior.add_parameter('E22',hobo.Normal(E2,(2*E2_diff)**2),
-        model.params['Ereverse']+e0_buffer,model.params['Estart']-e0_buffer)
-prior.add_parameter('k01',hobo.Uniform(),0,10000)
-prior.add_parameter('k02',hobo.Uniform(),0,10000)
-prior.add_parameter('k11',hobo.Uniform(),0,10000)
-prior.add_parameter('k12',hobo.Uniform(),0,10000)
-prior.add_parameter('k21',hobo.Uniform(),0,10000)
-prior.add_parameter('k22',hobo.Uniform(),0,10000)
-prior.add_parameter('gamma',hobo.Uniform(),0.1,10.0)
-
-print 'before cmaes, parameters are:'
-names = prior.get_parameter_names()
-v = np.zeros(len(names))
-for i in range(len(names)):
-    v[i] = model.params[names[i]]
-    print names[i],': ',model.params[names[i]]
-
-print 'param loglikelihood is currently',prior.log_likelihood(v,names)
-
-#
-
-v = [351.799992423,
-  4374.66433409,
-  6948.72148094,
-  -0.0171315128634,
-  -0.587492063523,
-  5965.6272421,
-  8.53884480798,
-  9.13625737772,
-  13.6671653148,
-  2184.96104188,
-  14.0741162244,
-  885.199106042,
-  0.808733615878]
-names = ['k01',
+names = ['E01',
+         'E02',
+         'E11',
+         'E12',
+         'E21',
+         'E22',
+         'k01',
          'k02',
-        'k21',
-        'E21',
-        'E22',
-        'k12',
-        'E11',
-        'E12',
-        'E02',
-        'k11',
-        'E01',
-        'k22',
-        'gamma']
-model.set_params_from_vector(v,names)
+         'k11',
+         'k12',
+         'k21',
+         'k22',
+         'gamma']
 
-#hobo.fit_model_with_cmaes(data,model,prior,IPOP=[10,20,40,80,160])
-#hobo.fit_model_with_cmaes(data,model,prior)
+e0_buffer = 0.1*(poms_model.params['Estart'] - poms_model.params['Ereverse'])
+max_current = np.max(data.current)
+max_k0 = poms_model.non_dimensionalise(10000,'k01')
+lower_bounds = [poms_model.params['Ereverse']+e0_buffer,
+                poms_model.params['Ereverse']+e0_buffer,
+                poms_model.params['Ereverse']+e0_buffer,
+                poms_model.params['Ereverse']+e0_buffer,
+                poms_model.params['Ereverse']+e0_buffer,
+                poms_model.params['Ereverse']+e0_buffer,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0.1,
+                0.005*max_current]
 
-print 'after cmaes, parameters are:'
-names = prior.get_parameter_names()
-for name in prior.get_parameter_names():
-    print name,': ',model.params[name]
+upper_bounds = [poms_model.params['Estart']-e0_buffer,
+                poms_model.params['Estart']-e0_buffer,
+                poms_model.params['Estart']-e0_buffer,
+                poms_model.params['Estart']-e0_buffer,
+                poms_model.params['Estart']-e0_buffer,
+                poms_model.params['Estart']-e0_buffer,
+                max_k0,
+                max_k0,
+                max_k0,
+                max_k0,
+                max_k0,
+                max_k0,
+                5,
+                0.03*max_current]
 
-print 'after cmaes, dim parameters are:'
+priors = []
+E0 = 0.5*(poms_model.params['E01'] + poms_model.params['E02'])
+E0_diff = 1
+priors.append(pints.NormalPrior(E0,(2*E0_diff)**2))
+priors.append(pints.NormalPrior(E0,(2*E0_diff)**2))
+E1 = 0.5*(poms_model.params['E11'] + poms_model.params['E12'])
+E1_diff = 1
+priors.append(pints.NormalPrior(E1,(2*E1_diff)**2))
+priors.append(pints.NormalPrior(E1,(2*E1_diff)**2))
+E2 = 0.5*(poms_model.params['E21'] + poms_model.params['E22'])
+E2_diff = 1
+priors.append(pints.NormalPrior(E2,(2*E2_diff)**2))
+priors.append(pints.NormalPrior(E2,(2*E2_diff)**2))
+priors.append(pints.UniformPrior(lower_bounds[6:14],upper_bounds[6:14]))
 
-names = prior.get_parameter_names()
-for name in prior.get_parameter_names():
-    print name,': ',model.dim_params[name]
+# Load a forward model
+pints_model = electrochemistry.PintsModelAdaptor(poms_model,names)
 
+# Create an object with links to the model and time series
+problem = pints.SingleSeriesProblem(pints_model, data.time, data.current)
 
+# Create a log-likelihood function scaled by n
+log_likelihood = pints.ScaledLogLikelihood(pints.UnknownNoiseLogLikelihood(problem))
 
-I,t = model.simulate(use_times=data.time)
-t = np.array(t)
-F = (fft.fft(I),fft.fft(data.current))
-dt = t[1]-t[0]
-signal_f = model.params['omega']/(2*pi)
-ia = np.array([1,2,3,4,5,6,7,8])
-hs = ia*signal_f
-lowcut = hs - signal_f/4
-highcut = hs + signal_f/4
-f,axs = plt.subplots(4,2,figsize = (10,13))
-fabs,axs_abs = plt.subplots(4,2,figsize = (10,13))
-colors = ['blue','red']
+# Create a uniform prior over both the parameters and the new noise variable
+prior = pints.ComposedPrior(*priors)
 
-weights = np.zeros(len(I))
-nyq = 0.5/dt
-mod = 1
-T_0 = model.T0
-I_0 = model.I0
-for i in range(len(ia)):
-    print 'doing subplot',int(i/2),',',i%2
-    cfreq = int(len(F[0]) * hs[i]/nyq/2)
-    band_low = int(lowcut[i]/nyq*len(F[0])/2)
-    band_high = int(highcut[i]/nyq*len(F[0])/2)
-    weights[:] = 0.0
-    weights[band_low:band_high] = 2.0
-    ax = axs[int(i/2),i%2]
-    ax_abs = axs_abs[int(i/2),i%2]
-    for color,FI in zip(colors,F):
-        Ih = FI*weights
-        Ih = np.concatenate((Ih[cfreq:],Ih[0:cfreq]))
-        Ih = fft.ifft(Ih)
+# Create a Bayesian log-likelihood (prior * likelihood)
+score = pints.BayesianLogLikelihood(prior, log_likelihood)
 
-        if len(I)%2 == 0:
-            ax.plot(t[0::mod]*T_0,np.real(Ih[0::mod])*I_0*1e6,c=color,ls='-')
-            ax.plot(t[0::mod]*T_0,np.imag(Ih[0::mod])*I_0*1e6,c=color,ls='--')
-            ax_abs.plot(t[0::mod]*T_0,np.abs(Ih[0::mod])*I_0*1e6,c=color,ls='-')
-        else:
-            ax.plot(T_0*t[0::mod],np.real(Ih[0::mod])*I_0*1e6,c=color,ls='-')
-            ax.plot(T_0*t[0::mod],np.imag(Ih[0::mod])*I_0*1e6,c=color,ls='--')
-            ax_abs.plot(T_0*t[0::mod],np.abs(Ih[0::mod])*I_0*1e6,c=color,ls='-')
+# Select some boundaries
+boundaries = pints.Boundaries(lower_bounds,upper_bounds)
 
-    if i%2==1:
-        ax.set_ylabel(r'$|I_{%dth}| \, (\mu A)$'%ia[i])
-       #ax.set_yticks([])
-    else:
-        ax.set_ylabel(r'$I_{%dth} \, (\mu A)$'%ia[i])
-    if int(i/2)<4:
-        ax.set_xticks([])
-    else:
-        ax.set_xlabel('$t \, (s)$')
-                                                                                                        #axs[0].legend()
-f.tight_layout()
-f.savefig('fit_pom_harmonics.pdf')
-fabs.tight_layout()
-fabs.savefig('fit_pom_harmonics_abs.pdf')
+# Perform an optimization with boundaries and hints
+x0 = [E0,E0,E1,E1,E2,E2] \
+        + [0.5*(u-l) for l,u in zip(lower_bounds[6:14],upper_bounds[6:14])]
+sigma0 = [0.25*(h-l) for l,h in zip(lower_bounds,upper_bounds)]
 
 
-plt.figure()
-plt.plot(t,I,alpha=0.5)
-plt.plot(data.time,data.current,alpha=0.5)
-plt.savefig('fit_pom.pdf')
-plt.close()
+print('log like at x0: ')
+print(log_likelihood(x0))
+
+print('prior at x0: ')
+print(prior(x0))
+
+print('Score at x0: ')
+print(score(x0))
+
+found_parameters, found_solution = pints.cmaes(
+    score,
+    boundaries,
+    x0,
+    sigma0,
+    )
 
 
+print('Found solution:          x0:' )
+for k, x in enumerate(found_parameters):
+    print(pints.strfloat(x) + '    ' + pints.strfloat(x0[k]))
 
+
+print('Score at found_parameters: ')
+print(found_solution)
+print(score(found_parameters))
